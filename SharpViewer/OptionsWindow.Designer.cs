b52310416fb22b519247ca3d100465eb2fa43f71
joshua bishop2 <joshua.bishop2@student.centralia.edu>
2021-03-01 10:59:20 -0800

    Added basic functionality for the OptionsWindow

--- SharpViewer/OptionsWindow.Designer.cs
+++ SharpViewer/OptionsWindow.Designer.cs
@@ -29,12 +29,70 @@ namespace SharpViewer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splashscreenEnabled = new System.Windows.Forms.CheckBox();
+            this.keepDirectory = new System.Windows.Forms.CheckBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // splashscreenEnabled
+            // 
+            this.splashscreenEnabled.AutoSize = true;
+            this.splashscreenEnabled.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splashscreenEnabled.Location = new System.Drawing.Point(99, 57);
+            this.splashscreenEnabled.Name = "splashscreenEnabled";
+            this.splashscreenEnabled.Size = new System.Drawing.Size(227, 24);
+            this.splashscreenEnabled.TabIndex = 0;
+            this.splashscreenEnabled.Text = "Show Splashscreen on start";
+            this.splashscreenEnabled.UseVisualStyleBackColor = true;
+            // 
+            // keepDirectory
+            // 
+            this.keepDirectory.AutoSize = true;
+            this.keepDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keepDirectory.Location = new System.Drawing.Point(99, 91);
+            this.keepDirectory.Name = "keepDirectory";
+            this.keepDirectory.Size = new System.Drawing.Size(258, 24);
+            this.keepDirectory.TabIndex = 1;
+            this.keepDirectory.Text = "Remember last opened directory";
+            this.keepDirectory.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(365, 176);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // OptionsWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "OptionsWindow";
+            this.ClientSize = new System.Drawing.Size(452, 211);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.keepDirectory);
+            this.Controls.Add(this.splashscreenEnabled);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "OptionsWindow";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Options";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OptionsWindow_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox splashscreenEnabled;
+        private System.Windows.Forms.CheckBox keepDirectory;
+        private System.Windows.Forms.Button btnClose;
     }
 }
